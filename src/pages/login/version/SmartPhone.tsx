5a059dee8a837dcd5eb6898a2071726dc25e44a6
Flori <floriandollani15@gmail.com>
2023-07-27 21:30:46 +0200

    BISMILAH fillimi projektit

---
+++ src/pages/login/version/SmartPhone.tsx
@@ -0,0 +1,23 @@
+const SmartPhone = () => {
+  return (
+    <main className="phoneLogin">
+      <form>
+        <figure className="logo&text">
+          <img src="" alt="" />
+          <h1>Page title</h1>
+        </figure>
+        <div className="phLogin-btnContainer" role="group">
+          <button>Vazhdo me Facebook</button>
+          <button>Vazhdo me Google Acc.</button>
+          <div className="phLogin-secondBtnCont" role="group">
+            <button>Ringarko</button>
+            <button>Tutorial</button>
+          </div>
+        </div>
+        <h3>Liçensa dhe termat e përdorimit</h3>
+      </form>
+    </main>
+  );
+};
+
+export default SmartPhone;
