59f801a2dcb0741097c25e50c3cfca622f2cebcf
Flori <floriandollani15@gmail.com>
2023-08-02 11:43:42 +0200

    InshaaAllah mbarova edhe me stilimet e loginPage per max 600px

--- src/pages/login/version/SmartPhone.tsx
+++ src/pages/login/version/SmartPhone.tsx
@@ -1,20 +1,35 @@
+import Logo from "../../../assets/images/logo.png";
+import { BiLogoFacebook as FbLogo } from "react-icons/bi";
+import { PiGoogleChromeLogoBold as GoogleLogo } from "react-icons/pi";
+
 const SmartPhone = () => {
   return (
     <main className="phoneLogin">
       <form>
         <figure className="logo&text">
-          <img src="" alt="Logo" loading="lazy" />
+          <img src={Logo} alt="Logo" loading="lazy" />
           <h1>Gjeniu Buxhetor</h1>
         </figure>
         <div className="phLogin-btnContainer" role="group">
-          <button>Vazhdo me Facebook</button>
-          <button>Vazhdo me Google Acc.</button>
+          <button>
+            <span>
+              <FbLogo />
+            </span>{" "}
+            Vazhdo me Facebook
+          </button>
+          <button>
+            {" "}
+            <span>
+              <GoogleLogo />
+            </span>{" "}
+            Vazhdo me Google Acc.
+          </button>
           <div className="phLogin-secondBtnCont" role="group">
             <button>Ringarko</button>
             <button>Tutorial</button>
           </div>
         </div>
-        <h3>Liçensa dhe termat e përdorimit</h3>
+        <h3>Liçensa dhe termat e përdorimit të aplikacionit Privacy Policy</h3>
       </form>
     </main>
   );
