d243a2d6b2dfc57672bb5d91c43a5bff9041b554
Flori <floriandollani15@gmail.com>
2023-08-03 10:17:21 +0200

    InshaaAllah mbarova stilimet per login max 1199

--- src/pages/login/version/Desktop.tsx
+++ src/pages/login/version/Desktop.tsx
@@ -1,27 +1,49 @@
-import React from "react";
+import miniDescIcon from "../../../assets/images/icons/miniDescpIcon.png";
+import Logo from "../../../assets/images/logo.png";
+import { BiLogoFacebook as FbLogo } from "react-icons/bi";
+import { PiGoogleChromeLogoBold as GoogleLogo } from "react-icons/pi";
 
 const Desktop = () => {
   return (
     <main className="desktLogin">
       <form>
         <figure className="topMinDesc">
-          <img src="" alt="Icon for mini description" loading="lazy" />
-          <h3>Mini description for the web</h3>
+          <img
+            src={miniDescIcon}
+            alt="Icon for mini description"
+            loading="lazy"
+          />
+          <h3>Menaxhim Financash</h3>
         </figure>
-        <figure className="logo&text">
-          <img src="" alt="Logo" loading="lazy" />
+        <figure className="logoDHtext">
+          <img src={Logo} alt="Logo" loading="lazy" />
           <h1>Gjeniu Buxhetor</h1>
         </figure>
         <div className="dskLogin-btnContainer" role="group">
-          <button>Vazhdo me Facebook</button>
-          <button>Vazhdo me Google Acc.</button>
+          <button>
+            {" "}
+            <span>
+              <FbLogo />
+            </span>
+            Vazhdo me Facebook
+          </button>
+          <button>
+            <span>
+              <GoogleLogo />
+            </span>
+            Vazhdo me Google Account
+          </button>
           <div className="dskLogin-secondBtnCont" role="group">
             <button>Ringarko</button>
             <button>Tutorial</button>
           </div>
         </div>
-        <h3>Liçensa dhe termat e përdorimit</h3>
+        <h3>Liçensa dhe termat e përdorimit të webit Privacy Policy</h3>
       </form>
+      <div
+        className="bckgLoginDesk"
+        aria-label="Background image for login page"
+      ></div>
     </main>
   );
 };
